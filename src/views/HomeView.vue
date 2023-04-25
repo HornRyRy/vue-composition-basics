5f4ff0b244c5ff9fd2d58274c9fd6c8e67dcc4ac
Ryan Horn <rj.horn@gmail.com>
2023-04-25 13:11:12 -0600

    data types data binding

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,91 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h3>{{ counterData.title }}:</h3>
+    <div>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{ counterData.count }}</span>
+      <button @click="increaseCounter" class="btn">+</button>
+    </div>
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input v-model="counterData.title" type="text">
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref, reactive } from 'vue'
+
+const counter = ref(0)
+const counterTitle = ref('My Counter')
+
+const counterData = reactive({
+  count: 0,
+  title: 'My Counter'
+})
+
+const increaseCounter = () =>{
+  counter.value++
+}
+const decreaseCounter = () =>{
+  counter.value--
+}
+</script>
+
+<!-- <script>
+export default{
+  data(){
+    return{
+      counter: 0
+    }
+  },
+  methods:{
+    increaseCounter(){
+      this.counter++
+    },
+    decreaseCounter(){
+      this.counter--
+    }
+  }
+}
+</script> -->
+
+<!-- <script>
+import { ref } from 'vue'
+
+export default{
+  setup(){
+    const counter = ref(0)
+
+    const increaseCounter = () =>{
+      counter.value++
+    }
+
+    const decreaseCounter = () =>{
+      counter.value--
+    }
+
+    return{
+      counter,
+      increaseCounter,
+      decreaseCounter
+    }
+  }
+}
+</script> -->
+
+
+
+<style>
+.home{
+  text-align: center;
+  padding: 20px;
+}
+.btn, .counter {
+  font-size: 40px;
+  margin: 10px;
+}
+.edit{
+  margin-top: 60px;
+}
+</style>
