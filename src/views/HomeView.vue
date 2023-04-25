3fc4acd227a488c538e8794f5c3434829c4adfdd
Ryan Horn <rj.horn@gmail.com>
2023-04-25 13:28:20 -0600

    adding computed properties

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="home">
+    <h2>{{ appTitle }}</h2>
     <h3>{{ counterData.title }}:</h3>
     <div>
-      <button @click="decreaseCounter" class="btn">-</button>
+      <button @click="decreaseCounter(1)" class="btn">-</button>
+      <button @click="decreaseCounter(2)" class="btn">--</button>
       <span class="counter">{{ counterData.count }}</span>
-      <button @click="increaseCounter" class="btn">+</button>
+      <button @click="increaseCounter(1)" class="btn">+</button>
+      <button @click="increaseCounter(2, $event)" class="btn">++</button>
+      <!-- Notice in the x2 counter, I have access to the event object via $event -->
     </div>
+
+    <p>This counter is {{ oddOrEven }}</p>
+
     <div class="edit">
       <h4>Edit counter title:</h4>
       <input v-model="counterData.title" type="text">
@@ -14,21 +21,32 @@
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue'
+import { ref, reactive, computed } from 'vue'
 
-const counter = ref(0)
-const counterTitle = ref('My Counter')
+//// non reactive data
+const appTitle = 'My Ok Counter App'
+
+//// refs
+// const counter = ref(0)
+// const counterTitle = ref('My Counter')
 
 const counterData = reactive({
   count: 0,
   title: 'My Counter'
 })
 
-const increaseCounter = () =>{
-  counter.value++
+const oddOrEven = computed(() =>{
+  if (counterData.count % 2 === 0) return 'even'
+  else return 'odd'
+})
+
+const increaseCounter = (amount, e) =>{
+  // here we are using the event object from the button and passing it here
+  console.log(e)
+  counterData.count += amount
 }
-const decreaseCounter = () =>{
-  counter.value--
+const decreaseCounter = (amount) =>{
+  counterData.count -= amount
 }
 </script>
 
